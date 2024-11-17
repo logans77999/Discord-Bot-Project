8f145792db70117253f7c6256efdc057f8a502d7
logans77999 <logans77999@yahoo.com>
2024-11-16 16:17:09 -0800

    discord bot setup

---
+++ main.py
@@ -0,0 +1,11 @@
+import discord
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run('MTMwNzQ5MjEyNzE4NTQzNjczNQ.GWmkno.AW1KiloEVsuobhuI_DWn8aLjDTefM1bnUQj0Jw')
