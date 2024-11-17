463fe88ddbd3aa462079c4f2b93dbada9a717de9
logans77999 <125546571+logans77999@users.noreply.github.com>
2024-11-16 16:35:12 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = Client(intents=intents)
-client.run('MTMwNzQ5MjEyNzE4NTQzNjczNQ.GWmkno.AW1KiloEVsuobhuI_DWn8aLjDTefM1bnUQj0Jw')
+client.run('MTMwNzQ5MjEyNzE4NTQzNjczNQ.GWmkno.AW1KiloEVsuobhuI_DWn8aLjDTefM1bnUQj0Jw') #unique key, this is not a real one
