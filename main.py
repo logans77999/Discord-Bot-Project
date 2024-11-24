f8a6bdad412b294582c3b79c5728516fa0e8a7ba
logans77999 <logans77999@yahoo.com>
2024-11-23 22:17:16 -0800

    edits

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import discord
 
 class Client(discord.Client):
+
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        
+        if message.content.startswith('hello') or message.content.startswith('Hello'):
+            await message.channel.send(f'Hi there {message.author}')
+
+    async def on_member_join(self, member):
+        await client.channel.send(f'Welcome {member} to the Server!') #not working properly right now
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = Client(intents=intents)
-client.run('MTMwNzQ5MjEyNzE4NTQzNjczNQ.GWmkno.AW1KiloEVsuobhuI_DWn8aLjDTefM1bnUQj0Jw')
+client.run('')
